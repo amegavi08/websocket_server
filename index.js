ed00bf61361006fe0dde866c1267f1d30cb1bec2
amegavi08 <amegavifrancis@gmail.com>
2025-06-03 01:19:26 +0000

    nodejs simple websocket server setup

---
+++ index.js
@@ -0,0 +1,17 @@
+import { WebSocketServer } from "ws";
+import si from "systeminformation";
+
+const wss = new WebSocketServer({ port: 8060 });
+
+wss.on("connection", function connection(ws) {
+    ws.on("message", function message(data) {
+        console.log("received: %s", data);
+    });
+    ws.send("here we go with the WebSocket server!");
+
+    setInterval(async () => {
+        const cpuTemp = JSON.stringify(await si.currentLoad());
+        ws.send(cpuTemp);
+    }, 5000);
+
+});
